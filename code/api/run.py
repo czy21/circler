85ac81ce14976d91c5134e0709c0b9d8574c9ef6
Bruce CHEN <805899926@qq.com>
2021-07-05 00:14:26 +0800

    #

--- code/api/run.py
+++ code/api/run.py
@@ -1,5 +1,5 @@
 from src.app import app
-from src.view import user as user_view
+from src.view import user as user_view, job as job_view
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
--- code/api/src/view/user.py
+++ code/api/src/view/user.py
@@ -2,10 +2,5 @@ from src.app import app
 
 
 @app.route('/user/load', methods=["POST"])
-def load():
+def user_load():
     return '用户加载'
-
-
-@app.route('/user/upload', methods=["POST"])
-def upload():
-    return '用户上传'
