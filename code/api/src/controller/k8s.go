7b6752308fc8b482b0fdf835c5ceaf598e83449a
Bruce CHEN <805899926@qq.com>
2021-07-06 18:47:56 +0800

    #

--- code/api/src/controller/k8s.go
+++ code/api/src/controller/k8s.go
@@ -1,13 +1,16 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/czyhome/circler/src/config"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/net/context"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func PodList(c *gin.Context) {
-	pods, err := config.K8sClient.CoreV1().Pods("").List(context.TODO(),)
+	pods, _ := config.K8sClient.CoreV1().Pods("default").List(context.TODO(), metav1.ListOptions{})
+	fmt.Println(pods)
 
 }
 
