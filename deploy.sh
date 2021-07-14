8b6553e0cb5ca15463f6c68b3377512e845a1ab3
Bruce CHEN <805899926@qq.com>
2021-07-14 19:48:26 +0800

    #

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,7 @@ dir=$(cd "$(dirname "$0")"; pwd)
 temp=${dir}/___temp
 rm -rf ${temp} && mkdir -p ${temp}
 
-temp_api=${temp}/api
-api_main=${temp_api}/main.go
+api_path=${dir}/api/src
+api_main=${temp}/main.go
 
-code_api_path=code/api/
-#export GOPATH=${dir}/${code_api_path}
-
-echo ${api_main}
-cd code/api/src && go build -o ${api_main} && cd ${dir}
+cd ${api_path} && go build -o ${api_main} && cd ${dir}
