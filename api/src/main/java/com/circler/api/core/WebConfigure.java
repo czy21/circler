92a8b0d4ec933a0cbcad13964942761b0d3281ee
Bruce CHEN <805899926@qq.com>
2022-01-04 15:38:08 +0800

    #

--- api/src/main/java/com/circler/api/core/WebConfigure.java
+++ api/src/main/java/com/circler/api/core/WebConfigure.java
@@ -2,6 +2,9 @@ package com.circler.api.core;
 
 import com.circler.api.core.json.JacksonConfigure;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -65,6 +68,11 @@ public class WebConfigure implements WebMvcConfigurer {
         AntPathMatcher matcher = new AntPathMatcher();
         matcher.setCaseSensitive(false);
         configurer.setPathMatcher(matcher);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
     }
 
 }
